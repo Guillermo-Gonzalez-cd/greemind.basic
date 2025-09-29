0b08cb4a4b9ec0c90a7f220a3684952167aa43a3
Guillermo-Gonzalez-cd <domfenix215@gmail.com>
2025-09-29 04:24:15 -0600

    Ya funciona las sidebar de todas los apartados

---
+++ js/loadSidebard.js
@@ -0,0 +1,8 @@
+fetch('sidebar.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('sidebar-container').innerHTML = data;
+  })
+  .catch(error => {
+    console.error('Error al cargar el sidebar:', error);
+  });
